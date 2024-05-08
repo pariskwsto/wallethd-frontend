3584ae7dc02ede36cf03f6aa863d6bcacebaa9f1
Paris Kostopoulos <pariskwsto@gmail.com>
2024-05-08 19:06:38 +0300

    feat(*): add eslint and prettier rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,96 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    'react-refresh',
+    'react',
+    '@typescript-eslint',
+    'prettier',
+    'typescript-sort-keys',
+    'unused-imports',
+    'sort-export-all',
+    'sort-destructure-keys',
+    'simple-import-sort',
+  ],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
+    'react-refresh/only-export-components': [
+      'warn',
       { allowConstantExport: true },
     ],
+    // general
+    'no-console': 'warn',
+    // react
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    // eslint plugins
+    'typescript-sort-keys/string-enum': 'error',
+    'typescript-sort-keys/interface': 'error',
+    'sort-export-all/sort-export-all': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'sort-destructure-keys/sort-destructure-keys': [
+      2,
+      {
+        caseSensitive: true,
+      },
+    ],
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // Packages `react` related packages come first.
+          ['^react', '^@?\\w'],
+          // Internal packages.
+          ['^(@|components)(/.*|$)'],
+          // Side effect imports.
+          ['^\\u0000'],
+          // Parent imports. Put `..` last.
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          // Style imports.
+          ['^.+\\.?(css)$'],
+        ],
+      },
+    ],
+    // typescript
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
+    // prettier
+    'prettier/prettier': 'error',
+  },
+  overrides: [
+    {
+      files: ['src/**/index.{ts,tsx,js,jsx}'],
+      rules: {
+        'sort-export-all/sort-export-all': 'error',
+      },
+    },
+    {
+      files: ['**/*.stories.*'],
+      rules: {
+        'import/no-anonymous-default-export': 'off',
+      },
+    },
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 };
