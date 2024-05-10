1bfa9b2f5b2f81cc675ece9bad94d6ee0b98385c
Paris Kostopoulos <pariskwsto@gmail.com>
2024-05-10 20:00:23 +0300

    feat(*): add react-query library

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import { GlobalStyle } from './GlobalStyle';
 import { AppRouter } from './router';
 
+const queryClient = new QueryClient();
+
 export const App = (): JSX.Element => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyle />
       <AppRouter />
-    </>
+      <ReactQueryDevtools initialIsOpen={false} position="right" />
+    </QueryClientProvider>
   );
 };
